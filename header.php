7f54678a5a19e9425a25e666a395f0da8ae2c325
Liisa Mikola <70939470+liismik@users.noreply.github.com>
2020-10-30 00:36:32 +0200

    Add files via upload

---
+++ header.php
@@ -0,0 +1,8 @@
+<!DOCTYPE html>
+<html lang="et">
+<head>
+	<meta charset="utf-8">
+	<title>Arbylis</title>
+	<link rel="icon" type="image/vnd.microsoft.icon"
+     href="file:///C:/Users/liisa/Downloads/favicon.ico" />
+</head>
