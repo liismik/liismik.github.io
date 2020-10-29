36a620bfcb770069b6a7fe6a09e0b73a776d6651
Liisa Mikola <70939470+liismik@users.noreply.github.com>
2020-10-30 00:41:10 +0200

    Delete header.php

--- header.php
+++
@@ -1,8 +0,0 @@
-<!DOCTYPE html>
-<html lang="et">
-<head>
-	<meta charset="utf-8">
-	<title>Arbylis</title>
-	<link rel="icon" type="image/vnd.microsoft.icon"
-     href="https://love2dev.com/img/2000px-instagram_logo_2016svg-2000x2000.png" />
-</head>
