681814f113044ad47cfa278335aca412c72465c3
Liisa Mikola <70939470+liismik@users.noreply.github.com>
2020-10-30 00:39:07 +0200

    Update header.php

--- header.php
+++ header.php
@@ -4,5 +4,5 @@
 	<meta charset="utf-8">
 	<title>Arbylis</title>
 	<link rel="icon" type="image/vnd.microsoft.icon"
-     href="https://www.google.com/search?q=favicon&sxsrf=ALeKk034omOrq8XiNL2wXLKjFNu1k11_dg:1604011064324&source=lnms&tbm=isch&sa=X&ved=2ahUKEwjeo9Ht7trsAhWNlYsKHQb3AEoQ_AUoAXoECAUQAw&biw=1920&bih=937#imgrc=UD7W92kGXjSw-M" />
+     href="https://love2dev.com/img/2000px-instagram_logo_2016svg-2000x2000.png" />
 </head>
